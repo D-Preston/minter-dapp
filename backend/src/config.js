9cbddd8419d6dfc5b790656b2b329fd7f6e59677
tosinobasa <105947723+tosinobasa@users.noreply.github.com>
2022-09-21 11:44:07 +0100

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -73,8 +73,8 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-09-20T22:30:48+00:00"; // This is required. Eg: 2022-09-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-09-20T22:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = "1000"; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = '1000'; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xC29F0e2d497DF8996bC63DCc2872FA5391A683db"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xaD1Eb4815DD9dBEfBD9749c106e6b2a30de04756"; // Replace with your own contract address
 const chain = 'polygon'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the AfrikaGirls NFT Project!!";
+const welcome_h1 = "AfrikaGirls";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The AfrikaGirls NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The AfrikaGirls is an NFT Project based on 999 Lagos girls.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
