6b0f72a36fb2fea48d5495f18b2a8244d6e1f1ae
tosinobasa <105947723+tosinobasa@users.noreply.github.com>
2022-09-21 12:11:33 +0100

    2update

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const MINT_PRICE = 10; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, 
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-20T22:30:48+00:00"; // This is required. Eg: 2022-09-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = '2022-09-21T13:30:48+00:00'; // This is required. Eg: 2022-09-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
